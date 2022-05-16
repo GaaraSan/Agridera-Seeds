c9ba2b5a48129fb0e7d4f924b06db70cd76fc64a
GaaraSan <87905938+GaaraSan@users.noreply.github.com>
2022-05-16 22:57:33 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+        products:[
+            {id:1, title:"Abkhaz", short_text:"Not too sweet, with a refreshing sourness and a pronounced aroma", image:"Abkhaz.jpeg", desc:"Not too sweet, with a refreshing sourness and a pronounced aroma. The peel of the 'Abkhazians' is light, yellow, the flesh is practically pitted."},
+            {id:2, title:"Turkish", short_text:"Small fruits, with a yellowish peel and pulp with a sour taste. Contains many bones.", image:"Turkish.jpeg", desc:"Small fruits, with a yellowish peel and pulp with a sour taste. Contains many bones."},
+            {id:3, title:"Spanish", short_text:"Large, often 'loose' fruits with a thick peel with large pores", image:"Spanish.jpeg", desc:"Large, often 'loose' fruits with a thick peel with large pores. Juicy and sweet, but they usually have a lot of seeds."},
+            {id:4, title:"Moroccan", short_text:"The fruits are not round, but slightly flattened in shape. Sweet, but not sugary flesh", image:"Moroccan.jpeg", desc:"The fruits are not round, but slightly flattened in shape. Sweet, but not sugary flesh with a thin bright orange, sometimes even reddish skin. The skin is thin and easy to peel. There are practically no bones."},
+            {id:5, title:"Clementine (Algerian tangerine)", short_text:"Fruits juicy, with a pleasant tart taste, usually without seeds.", image:"Clementine_Algerian_tangerine.jpeg", desc:"Fruits with a yellow-orange skin that is easy to peel, quite large, juicy, with a pleasant tart taste, usually without seeds."}
+        ]
+    },
+    mounted:function(){
+        console.log(window.localStorage.getItem('prod'));
+    },
+    methods:{
+        addItem:function(id){
+            window.localStorage.setItem('prod',id);
+        }
+    }
+});
